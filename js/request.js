5c115fcddb5805230e2e0a5af31bb400d94f71b0
ÁõÎÀÐÂ <2415322960@qq.com>
2020-11-09 14:34:53 +0800

    node爬取博客园文章并生成excel

--- js/request.js
+++ js/request.js
@@ -1,29 +1,36 @@
 const superagent = require('superagent')
-// const async = require('async')
+const async = require('async')
+const cheerio = require('cheerio')
+
+
+async function getBKY(url) {
+    const res = await superagent.post(url)
+    let str = (JSON.stringify(res)).replace(/\\/gi, '')
+    return new Promise((resolve, reject) => {
+        resolve(str)
+    })
+}
 
 //博客园文章
-function bky(url) {
+async function bky(url) {
     let Arr = []
     let regTitle = /target="_blank">.*?(\/a>)/gi
     let regUrl = /(https|http).*?(\.html)/gi
-    superagent.get(url).end((err, res) => {
-        let reg = /<a class="post-item-title".*?(\/a>)/gi
-        let str = JSON.stringify(res)
-        str = str.replace(/\\/gi, '')
-        let aArr = str.match(reg)
-        aArr.forEach(item => {
-            let a = {}
-            let value = item
-            let urlValue = value.match(regUrl)[0]
-            let titleValue = (value.match(regTitle)[0]).substring(16, (item.match(regTitle)[0]).indexOf('</a>'))
-            a.title = titleValue
-            a.url = urlValue
-            Arr.push(a)
-        })      
-        // return Arr
-        console.log(Arr)  
+    let res = await getBKY(url)
+    let reg = /<a class="post-item-title".*?(\/a>)/gi
+    let str = JSON.stringify(res)
+    str = str.replace(/\\/gi, '')
+    let aArr = str.match(reg)
+    aArr.forEach(item => {
+        let a = []
+        let urlValue = item.match(regUrl)[0]
+        let titleValue = (item.match(regTitle)[0]).substring(16, (item.match(regTitle)[0]).indexOf('</a>'))
+        a.push(titleValue)
+        a.push(urlValue)
+        Arr.push(a)
     })
+    return Arr
 }
-let url = 'https://www.cnblogs.com/'
-bky(url)
+
+module.exports = {bky}
 
--- js/xlsx.js
+++ js/xlsx.js
@@ -1,14 +1,30 @@
 const xlsx = require('node-xlsx')
-
+const async = require('async')
 const fs = require('fs')
 
+let bky = require('./request')
 let writeXls = require('./writeXls')
 
 let data = []
-let title =['id','name','sex','age','address']
+let title = ['文章标题', '文章地址']
 data.push(title)
-let data1 = ['1','lwx','男','23','1111111111']
-data.push(data1)
 
 
-writeXls.writeXls('js/sheet2.xlsx',data,'sheet1')
+function getData(i, max) {
+    if (i >= max) return
+    let bkyUrl = 'https://www.cnblogs.com/?CategoryId=808&CategoryType=SiteHome&ItemListActionName=AggSitePostList&PageIndex=' + (i + 1) + '&ParentCategoryId=0&TotalPostCount=4000'
+    bky.bky(bkyUrl).then(res => {
+        data = data.concat(res)
+        console.log(i+1+'页录入数组')
+        if (i == max - 1) {
+            console.log('excel表生成')
+            writeXls.writeXls('js/sheet2.xlsx', data, 'sheet1')
+        }
+        i++
+        getData(i,max)
+    })
+}
+getData(0, 200)
+
+
+
