9cd47a113b8f29f1758ccfb243dee16fd1b35cd9
ÁõÎÀÐÂ <2415322960@qq.com>
2020-11-17 17:33:20 +0800

    test

--- js/aysnc_bky.js
+++ js/aysnc_bky.js
@@ -4,14 +4,14 @@ const cheerio = require('cheerio')
 
 //csdn-Web 开发论坛-数据
 let data = []  //excel数据
-let title = ['类目', '标题', '地址','阅读','推荐','时间']
+let title = ['类目', '标题', '地址', '阅读', '推荐', '时间']
 data.push(title)
 
 
 function run(start, end) {
     if (start >= end) return
     let csdnArr = [] //每页的数据
-    let url = `https://kb.cnblogs.com/${(start+1)}/`
+    let url = `https://kb.cnblogs.com/${(start + 1)}/`
     superagent.get(url).then(res => {
         let $ = cheerio.load(res.text)
         $('.aiticle_item>.message_info').each((i, item) => {
@@ -20,8 +20,8 @@ function run(start, end) {
             arr.push($(item).children(".msg_title").children("p").children(".classify_name").text().trim())
             arr.push($(item).children(".msg_title").children("p").children('a').text().trim())
             arr.push('https://kb.cnblogs.com' + $(item).children(".msg_title").children("p").children('a').attr('href'))
-            arr.push($(item).children(".msg_tag").children('.view').text().trim().replace('阅读',''))
-            arr.push($(item).children(".msg_tag").children('.recommend').text().trim().replace('推荐',''))
+            arr.push($(item).children(".msg_tag").children('.view').text().trim().replace('阅读', ''))
+            arr.push($(item).children(".msg_tag").children('.recommend').text().trim().replace('推荐', ''))
             arr.push($(item).children(".msg_tag").children('.green').text().trim())
             csdnArr.push(arr)
         })
