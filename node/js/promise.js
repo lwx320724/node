db42e3c9467422e9b99bf99e235d60aeb0336374
ÁõÎÀÐÂ <2415322960@qq.com>
2021-01-18 17:54:58 +0800

    promise

--- node/js/promise.js
+++ node/js/promise.js
@@ -2,18 +2,57 @@ const PENDING = 'pending'
 const RESOLVED = 'resolved'
 const REJECTED = 'rejected'
 
+/**
+ *  
+ * 
+ * 
+ * 
+ * 
+ */
+class MyPromise {
+    status = PENDING;
+    value = undefined;
 
-class myPromise{
-    constructor(resolver){
-
-
-
-
-        this.state = PENDING;
-        this.data = undefined;
-        this.callbackQueue = [];
-
-
-        
+    constructor(resolver) {
+        let resolve = (val) => {
+            this.status = RESOLVED
+            this.value = val
+        }
+        let reject = (val) => {
+            this.status = REJECTED
+            this.value = val
+        }
+        resolver(resolve, reject)
     }
+
+
+    then = (callbackResolve, callbackReject) => {
+        if (callbackResolve) {
+            callbackResolve(this.value)
+        }
+
+        return this
+    }
+
+    catch = (callbackReject) => {
+        if (callbackReject && this.status === REJECTED) {
+            callbackReject(this.value)
+        }
+    }
+
+
+
+    static resolve = (value) => {
+        if (value instanceof this) return value
+        return new this(resolve => { resolve(value) })
+    }
+
+    static reject = (reason) => {
+        return new this((_, reject) => { reject(reason) })
+    }
+
+
 }
+
+
+MyPromise.resolve('MyPromise').then().then(res => { console.log(res) })
--- node/js/test.js
+++ node/js/test.js
@@ -1,72 +1,45 @@
-function Promise(fn) {
-    this.cbs = [];
+/*
+    自定义Promise
+*/
+(function (window) {
 
-    const resolve = (value) => {
-        setTimeout(() => {
-            this.data = value;
-            this.cbs.forEach((cb) => cb(value));
-        });
+
+    function Promise(executor) {
+
     }
 
-    fn(resolve.bind(this));
+    window.Promise = Promise
+
+})()
+
+
+/*
+    promise原型对象的then()
+*/
+
+Promise.prototype.then = function (onResolved, onRejected) {
+
 }
 
-Promise.prototype.then = function (onResolved) {
-    return new Promise((resolve) => {
-        this.cbs.push(() => {
-            const res = onResolved(this.data);
-            if (res instanceof Promise) {
-                res.then(resolve);
-            } else {
-                resolve(res);
-            }
-        });
-    });
-};
+/*
+    promise原型对象的catch
+*/
+Promise.prototype.catch = function (onRejected) {
 
-
-// new Promise((resolve) => {
-//     setTimeout(() => {
-//         resolve(1);
-//     }, 500);
-// })
-//     .then((res) => {
-//         console.log(res);
-//         return new Promise((resolve) => {
-//             setTimeout(() => {
-//                 resolve(2);
-//             }, 500);
-//         });
-//     })
-//     .then(console.log);
-
-
-
-
-
-class Parent1 {
-    constructor() {
-        this.name = 'parent1';
-    }
-    say() {
-        console.log(this.name);
-    }
 }
-class Child1 {
-    constructor() {
-        Parent1.call(this); //构造函数继承核心代码
-        this.type = 'child1';
-    }
+
+Promise.resolve = function (value) {
+
 }
-let child1_1 = new Child1()
+Promise.reject = function (value) {
 
-console.log(child1_1.name) // parent1
-console.log(child1_1.say()) // child1_1.say is not a function
+}
+Promise.all = function (value) {
 
+}
+Promise.race = function (value) {
 
-
-
-
+}
 
 
 
